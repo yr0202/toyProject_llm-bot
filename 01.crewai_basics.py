61da4969466237a54107b0a77f2f5a3caa51999b
yr0202 <yooram0202@naver.com>
2024-09-02 03:05:05 +0900

    LLM project settings & basic code write

---
+++ 01.crewai_basics.py
@@ -0,0 +1,68 @@
+from crewai import Crew, Agent, Task 
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model='phi3',
+    base_url='http://localhost:11434'
+)
+
+# Crew : 러닝크루 => N명 (조직)
+# Agent : 요원 => 1명 (조직원) , 여러개 만들 수 있음
+# Task : 미션 , 여러개 만들 수 있음
+
+user_question = input('편하게 질문해주세요 :)')
+
+book_agent = Agent(
+    role='책 구매 어시스턴트',
+    goal='고객이 어떤 상황인지 설명을 하면 해당 상황에 맞는 우리 서점에 있는 책을 소개합니다.',
+    backstory='당신은 우리 서점의 모든 책 정보를 알고있으며, 사람들의 상황에 맞는 책을 소개하는데 전문가입니다.',
+    llm=llm
+)
+
+recommand_book_task =  Task(
+    # description='고객의 상황에 맞는 최고의 추천 도서 제안하기',
+    description=user_question,
+    expected_output='고객의 상황에 맞는 5개의 도서를 추천해주고, 해당 책을 추천한 이유를 알려줘',
+    agent=book_agent,
+    output_file='recommand_book_task.md'
+)
+
+review_agent = Agent(
+    role='책 리뷰 어시스턴트',
+    goal='추천받은 책들의 도서에 대한 리뷰를 제공하고, 해당 도서에 대한 심도있는 평가를 제공합니다.',
+    backstory='당신은 우리 서점의 모든 책 정보를 알고있으며, 추천받은 책에 대한 전문가 수준의 리뷰를 제공합니다.',
+    llm=llm
+)
+
+review_task =  Task(
+    description='고객이 선택한 책에 대한 리뷰를 제공합니다.',
+    expected_output='고객이 선택한 책에 대한 리뷰를 제공합니다.',
+    agent=review_agent,
+    output_file='review_task.md'
+)
+
+# 요원과 미션을 관리
+crew = Crew(
+    agents=[book_agent, review_agent],
+    tasks=[recommand_book_task, review_task],
+    verbose=True
+)
+
+result = crew.kickoff()
+
+print(result)
+
+# python 01.crewai_basics.py 로 실행하기
+
+# 우리가 하려는 것. 배우고 있는 것
+# 파이썬을 활용한 LLM - ollma, CrewAI(AI agent-랜딩 페이지 제작)
+
+# 언어모델 핸들링하고나서 결과값을 자바서버에 내려줘야해.
+# LLM이 만들어낸 결과값을 어떻게 자바 서버에 내려주지?
+# - REST API(Python-FrameWork) => Java서버
+
+# Flask(쉬움,진입장벽 낮음), FastAPI(진입장벽이 있는 편) => (1) 깃헙 스타 (2) 구글 검색량
+# - AI기업. 메인 서버. AI 관련 인퍼런스(추론값) 값은 Python 백엔드로 내려주는 형태
+
+# 프론트엔드는 어떻게 개발하고 계세요? java-jinja template
+# 프론트엔드 프레임워크
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # yuram-llm-bot
 LLM chat bot to use CrewAI
+
+## 1. 프로젝트 세팅
+- VSC
+- Github 연동
+
+## 2. 프로젝트 구성
+- 가상환경을 설정 => Docker Container 개념 (공간을 분리해서 따로 관리하겠다)
+- python 3.8 열심히 개발했어요. 근데 우리가 배포할 서버가 3.3버전의 파이썬이다.
+    => 3.8 함수를 썼다면 3.3에는 없는 함수야 => 오류 => 배포하고 나서 알겠죠
+    => 로컬에서 작업하는 환경과 호스트 서버에서 작업하는 환경을 일치시켜 주기 위함
+    => Docker(Virtual Machine) // venv모듈을 사용해서 환경 설정을 해주도록 하겠음
+    => Docker는 환경을 동일하게 만들어줌
+
+> python3.10 -m venv .venv (강사님) => python -m venv .venv (유람컴)
+.venv (가상환경) : 실행시켜야함 => .venv/Scripts/activate (windows)
+
+## 3. 프로젝트
+
+### 3-1. Ollama 모델 + CrewAI
+
+(1) ollama 다운로드 : 언어모델을 로컬에서 돌릴 수 있도록 도와주는 프로그램
+
+(2) ollama를 통해서 llm 다운로드
+    ollama pull llama3.1 -> 이건 무거워서 가벼운 phi3으로
+    ollama pull phi3
+    ollama run phi3 -> 올라마 실행시키기, api가 우리 컴터에서 열리는거다
+
+(3) CrewAI 설치
+- 언어 모델의 API 관리를 편리하게 도와주는 라이브러리
+- 모델 - 클로드, 젬미니, GPT3.5, GPT4o ... -> import OpenAI //언어마다 SDK를 다운받아줘야함
+    => CrewAI, LangChain이 이미 다 SDK 구현을 끝내놓음
+- CrewAI가 LangChain보다 가벼워서 사용 
+
+> pip install crewai crewai-tools => To install crewAI, you need to have Python >=3.10 and <=3.13 installed on your system:
+> pip install langchain-ollama
+
+- pydantic : 파이썬에서 데이터 타입을 명시할 수 있게 해주는 라이브러리
+
+### 3-2. Flask 사용해서 기본적인 챗봇
